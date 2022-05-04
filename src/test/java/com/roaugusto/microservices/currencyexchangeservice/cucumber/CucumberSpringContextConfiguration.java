b4240e3d18087cda3bf7c9e38291924ec84f32c4
Rodrigo Santos <ro.augusto@gmail.com>
2022-05-04 17:06:41 -0300

    first commit

---
+++ src/test/java/com/roaugusto/microservices/currencyexchangeservice/cucumber/CucumberSpringContextConfiguration.java
@@ -0,0 +1,19 @@
+package com.roaugusto.microservices.currencyexchangeservice.cucumber;
+
+import com.roaugusto.microservices.currencyexchangeservice.CurrencyExchangeServiceApplicationH2;
+
+import org.springframework.boot.test.context.SpringBootContextLoader;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ContextConfiguration;
+
+import cucumber.api.java.Before;
+
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+@ContextConfiguration(classes = CurrencyExchangeServiceApplicationH2.class, loader = SpringBootContextLoader.class)
+public class CucumberSpringContextConfiguration {
+
+  @Before
+  public void setUp() {
+  }
+}
